0549bf5cb9f3015c916aec41cc60800248b091b5
Siddharth <90437355+JadugarKalakaboola@users.noreply.github.com>
2022-01-27 19:39:00 +0530

    Basic Bored Bot

---
+++ main.js
@@ -0,0 +1,45 @@
+localStorage.getItem("user")
+const botStart = document.querySelector(".bot-start")
+const botActive = document.querySelector(".bot-active")
+const userNameInput = document.querySelector(".user-name-input")
+const userNameBtn = document.querySelector(".user-name-btn")
+const activityBtn = document.querySelector(".activity-btn")
+
+activityBtn.addEventListener("click", function(){
+    fetch("https://apis.scrimba.com/bored/api/activity")
+    .then(response => response.json())
+    .then(data => {
+        document.querySelector(".new-idea").textContent = data.activity
+        activityBtn.textContent = "Get Another Activity"
+    })
+})
+
+if(localStorage.getItem("user")){
+    botStart.classList.add("none")
+    botActive.classList.remove("none")
+    document.querySelector(".head1").classList.add("none")
+    document.querySelector(".head2").classList.add("none")
+    document.querySelector(".head3").classList.remove("none")
+}
+userNameBtn.addEventListener("click", function(){
+    if(userNameInput){
+        let userName = userNameInput.value
+        localStorage.setItem("user", userName)
+        let x = localStorage.getItem("user")
+        document.querySelector(".user-name").textContent = x
+        document.querySelector(".head1").classList.add("none")
+        document.querySelector(".head2").classList.add("none")
+        document.querySelector(".head3").classList.remove("none")
+        botStart.classList.remove("none")
+    } 
+})
+
+botStart.addEventListener("click", function(){
+    botStart.classList.add("none")
+    botActive.classList.remove("none")
+})
+let x = localStorage.getItem("user")
+var y = document.querySelectorAll('.user-name');
+y.forEach(function(name){
+    name.textContent = x;
+});
